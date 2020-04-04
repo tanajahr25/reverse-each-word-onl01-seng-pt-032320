33fc320640e9d1f959b42500db3239a197378431
Tanajah Rosenbaum <tanajahr25@gmail.com>
2020-04-04 22:38:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,4 +4,16 @@
 # end
 
 def reverse_each_word(string)
-  
+   original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse
