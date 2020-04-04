b322d3315903c1f6edc9f7b91df962ac5f57417b
Tanajah Rosenbaum <tanajahr25@gmail.com>
2020-04-04 22:38:42 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,25 @@
+# def reverse_each_word(string)
+#   string.split("Hello there, and how are you?")
+#   puts reverse.string
+# end
+
+def reverse_each_word(string)
+   original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse
+     end
+  test_array.join(" ")
+end
+
+
+reverse_each_word("Hello there, and how are you?")
