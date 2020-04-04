fc9c11a6a057ce109223bc2636c63b444650670e
Tanajah Rosenbaum <tanajahr25@gmail.com>
2020-04-04 22:35:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-def reverse_each_word(string)
-  string.split("Hello there, and how are you?")
-  puts reverse.string
-end
+# def reverse_each_word(string)
+#   string.split("Hello there, and how are you?")
+#   puts reverse.string
+# end
